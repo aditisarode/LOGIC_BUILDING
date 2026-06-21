479293cf4046fe597b9f431e7ba1fc009ea5b098
aditisarode <aditids2006@gmail.com>
2026-06-21 20:29:39 +0530

    Problem 28 Solution

---
+++ program28.c
@@ -0,0 +1,21 @@
+//objective-> 
+
+#include<stdio.h>
+
+void Display(int iCnt,int n)
+{
+    for(iCnt=1;iCnt<=n;iCnt++)
+    {
+        printf("Jay Ganesh..\n");
+    }
+}
+int main()
+{
+    int iCnt=0;
+    int n;
+
+    printf("Enter number of times u want to print on the screen :\n");
+    scanf("%d",&n);
+
+    Display(iCnt,n);
+}
