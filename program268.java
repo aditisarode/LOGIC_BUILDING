656bc6a958908bdd2c05abf4c8900df7f3b4940f
aditisarode <aditids2006@gmail.com>
2026-06-29 17:41:27 +0530

    Problem 268 Solution

---
+++ program268.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class StringX
+{
+    public void Display(String str)   
+    {
+        System.out.println("Received string is :" +str);
+    }
+}
+class program268
+{
+    public static void main(String A[])
+    {
+      Scanner sobj = new Scanner(System.in);
+      String str = null;                            
+
+      program280 strobj = new program280();                    // object
+
+      System.out.println("Enter string :");
+      str = sobj.nextLine();
+
+      strobj.Display(str);
+
+      
+    }
+}
