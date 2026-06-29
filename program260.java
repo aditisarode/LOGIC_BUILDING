b3c1e2fb50af478744eee7e5a9933218ac5249b4
aditisarode <aditids2006@gmail.com>
2026-06-29 17:38:43 +0530

    Problem 260 Solution

---
+++ program260.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class program260
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+        String Arr = null;                  // this is reference not array
+
+        System.out.println("Enter string :");
+        Arr = sobj.nextLine();
+
+        System.out.println("Entered string :"+Arr);
+
+    }
+}
