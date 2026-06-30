89b28de6546cccd323bc5b7b12995778937589aa
aditisarode <aditids2006@gmail.com>
2026-06-30 17:59:58 +0530

    Problem 304 Solution

---
+++ program304.c
@@ -0,0 +1,21 @@
+
+#include <Stdio.h>
+
+int main()
+{
+    int No1 = 0;
+    int No2 = 0;
+    int Ans = 0;
+
+    printf("Enter the first number :\n");
+    scanf("%d",&No1);
+
+    printf("Enter the second number :\n");
+    scanf("%d",&No2);
+
+    Ans = No1 & No2;
+
+    printf("Result is : %d\n",Ans);
+
+    return 0;
+}
