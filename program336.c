4c78d7732b0ba208636471a57ee13514a4fe98c5
aditisarode <aditids2006@gmail.com>
2026-07-04 11:10:59 +0530

    Problem 336 Solution

---
+++ program336.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+typedef unsigned int UINT;
+
+// Position : 4
+UINT OffBit(UINT iNo, UINT iPos)
+{
+    UINT iMask = 0xFFFFFFF7;
+    UINT iResult = 0;
+
+    if(iPos < 1 || iPos > 32)
+    {
+        printf("Invalid bit position\n");
+        return iNo;
+    }
+    
+    iMask = iMask << (iPos - 1);
+
+    iResult = iNo ^ iMask;
+
+    return iResult;
+
+}
+int main()
+{
+    UINT iValue = 0, iRet = 0, iLocation = 0;
+
+    printf("Enter the number :\n");
+    scanf("%d",&iValue);
+
+    printf("Enter the bit position :\n");
+    scanf("%d",&iLocation);
+    
+    iRet = OffBit(iValue, iLocation);
+
+    printf("Updated Number : %d\n", iRet);
+   
+    return 0;
+}
