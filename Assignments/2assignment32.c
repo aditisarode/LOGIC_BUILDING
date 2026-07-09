bfa0f0adec6f09b2b2341c0686c61bdfbc049f27
aditisarode <aditids2006@gmail.com>
2026-07-09 14:03:24 +0530

    Problem 2 Solution

---
+++ Assignments/2assignment32.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int CountChar(char *str, char ch)
+{
+    int iCnt = 0;
+
+    while(*str != '\0')
+    {
+        if(*str == ch)
+        {
+            iCnt++;
+        }
+        str++;
+    }
+    return iCnt;
+}
+int main()
+{
+    char Arr[20];
+    char cValue;
+    int iRet = 0;
+    
+    printf("Enter string : \n");
+    scanf("%[^'\n']s", Arr);
+
+    printf("Enter the character : \n");
+    scanf(" %c",&cValue);
+
+    iRet = CountChar(Arr, cValue);
+
+    printf("Character frequency is %d ", iRet);
+
+    return 0;
+}
