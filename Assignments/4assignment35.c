2cf417a163ff5ba5431cd65fc3428ca13dbff4ad
aditisarode <aditids2006@gmail.com>
2026-07-13 22:59:42 +0530

    Problem 4 Solution

---
+++ Assignments/4assignment35.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+typedef unsigned int UINT;
+
+UINT ToggleBit(UINT iNo)
+{
+    UINT iMask = 0x00000240;
+   
+    iNo = iNo ^ iMask;
+
+    return iNo;
+
+}
+int main()
+{
+    UINT iValue = 0;
+    UINT iRet = 0;
+
+    printf("Enter the number : \n");
+    scanf("%u",&iValue);
+
+    iRet = ToggleBit(iValue);
+
+    printf("Updated Number : %d",iRet);
+
+    return 0;
+
+}
