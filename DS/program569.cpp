c3d4a1f0329bc20e3a347cf80b366c8f365d0b10
aditisarode <aditids2006@gmail.com>
2026-07-29 19:10:58 +0530

    Problem 569 Solution

---
+++ DS/program569.cpp
@@ -0,0 +1,121 @@
+#include <iostream>
+using namespace std;
+
+class Searching
+{
+    private:
+        int *Arr;
+        int iSize;
+
+    public:
+        Searching(int iNo);
+        ~Searching();
+
+        void Accept();
+        void Display();
+
+        bool LinearSearch(int iNo);
+        bool BiDirectionalSearch(int iNo);
+};
+
+Searching :: Searching(int iNo)       
+{
+    iSize = iNo;
+    Arr = new int[iSize];
+}
+
+Searching :: ~Searching()             
+{
+    delete []Arr;
+}
+
+void Searching :: Accept()
+{
+    int i = 0;
+    cout<<"Enter the elements : \n";
+
+    for(i = 0; i < iSize; i++)
+    {
+        cin>>Arr[i];
+    }
+}   
+
+void Searching :: Display()
+{
+    int i = 0;
+    cout<<"Elements of the array : \n";
+
+    for(i = 0; i < iSize; i++)
+    {
+       cout<<Arr[i]<<"\n";
+    }
+}   
+
+bool Searching ::LinearSearch(int iNo)
+{
+    bool bFlag = false;
+    int i = 0;
+
+    for(i = 0; i < iSize; i++)
+    {
+        if(Arr[i] == iNo)
+        {
+            bFlag = true;
+            break;
+        }
+    }
+
+    return bFlag;
+}
+
+bool Searching :: BiDirectionalSearch(int iNo)
+{
+    int iStart = 0, iEnd = 0;
+    bool bFlag = false;
+
+    iStart = 0;
+    iEnd = iSize - 1;
+
+    while(iStart <= iEnd)          
+    {
+        if(Arr[iStart] == iNo || Arr[iEnd] == iNo)
+        {
+            bFlag = true;
+            break;
+        }
+
+        iStart++;
+        iEnd--;
+    }
+
+    return bFlag;
+
+}
+
+int main()
+{
+    Searching sobj(5);
+
+    sobj.Accept();
+    sobj.Display();
+   
+    if(sobj.LinearSearch(30) == true)
+    {
+        cout<<"Element is present\n";
+    }
+    else
+    {
+        cout<<"Element is not present\n";
+    }
+
+    if(sobj.BiDirectionalSearch(30) == true)
+    {
+        cout<<"Element is present\n";
+    }
+    else
+    {
+        cout<<"Element is not present\n";
+    }
+    
+    return 0;
+}
