cf5e19418e0a4a7c5764bc5509694d5b5395ef39
aditisarode <aditids2006@gmail.com>
2026-07-30 22:29:38 +0530

    Problem 570 Solution

---
+++ DS/program570.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+using namespace std;
+
+template <class T>
+class Searching
+{
+    private:
+        T *Arr;
+        int iSize;
+
+    public:
+        Searching(int iNo);
+        ~Searching();
+
+        void Accept();
+        void Display();
+
+        bool LinearSearch(T iNo);
+};
+
+template <class T>
+Searching<T> :: Searching(int iNo)       
+{
+    iSize = iNo;
+    Arr = new T[iSize];
+}
+
+template <class T>
+Searching<T> :: ~Searching()             
+{
+    delete []Arr;
+}
+
+template <class T>
+void Searching<T> :: Accept()
+{
+    int i = 0;
+    cout<<"Enter the elements : \n";
+
+    for(i = 0; i < iSize; i++)
+    {
+        cin>>Arr[i];
+    }
+}   
+
+template <class T>
+void Searching<T> :: Display()
+{
+    int i = 0;
+    cout<<"Elements of the array : \n";
+
+    for(i = 0; i < iSize; i++)
+    {
+       cout<<Arr[i]<<"\n";
+    }
+}   
+
+template <class T>
+bool Searching<T> ::LinearSearch(T iNo)
+{
+    bool bFlag = false;
+    int i = 0;
+
+    for(i = 0; i < iSize; i++)
+    {
+        if(Arr[i] == iNo)
+        {
+            bFlag = true;
+            break;
+        }
+    }
+
+    return bFlag;
+}
+
+int main()
+{
+    Searching<int> sobj(5);
+
+    sobj.Accept();
+    sobj.Display();
+   
+    if(sobj.LinearSearch(30) == true)
+    {
+        cout<<"Element is present\n";
+    }
+    else
+    {
+        cout<<"Element is not present\n";
+    }
+    
+    return 0;
+}
