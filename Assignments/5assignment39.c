e90cae44c70f737d3514cf5c84fbfe4ba7153b43
aditisarode <aditids2006@gmail.com>
2026-08-04 22:44:24 +0530

    Problem 5 Solution

---
+++ Assignments/5assignment39.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+struct node
+{
+    int data;
+    struct node* next;
+
+};
+
+typedef struct node NODE;
+typedef struct node* PNODE;
+
+int CountLess(PNODE head, int X)
+{
+    int iCount = 0;
+
+    while(head != NULL)
+    {
+        if(head->data < X)
+        {
+            iCount++;
+        }
+
+        head = head->next;
+    }
+
+    return iCount;
+}
