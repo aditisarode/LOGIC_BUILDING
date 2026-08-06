cdef7d796bf46d125288b42f8f0a675477b79eb2
aditisarode <aditids2006@gmail.com>
2026-08-06 23:14:41 +0530

    Problem 3 Solution

---
+++ Assignments/3assignment42.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void Display()
+{
+    static int i = 5;
+
+    if(i > 0)
+    {
+        printf("%d\t", i);
+        i--;
+
+        Display();
+    }
+}
+
+int main()
+{
+    Display();
+
+    return 0;
+}
