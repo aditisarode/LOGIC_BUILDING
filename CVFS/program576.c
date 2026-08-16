a5d070ad77db2104cd30e790df4500b31a4b2d95
aditisarode <aditids2006@gmail.com>
2026-08-16 18:19:19 +0530

    CVFS Project

---
+++ CVFS/program576.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>            // only LINUX based OS
+
+int main()
+{
+    int fd = 0;
+
+    fd = open("Marvellous.txt", O_RDWR);
+
+    if(fd == -1)
+    {
+        printf("Unable to open file\n");
+    }
+    else
+    {
+        printf("File gets successfully opened with fd : %d\n", fd);
+
+        write(fd, "Jay Ganesh...", 13);
+
+        close(fd);
+    }
+
+    return 0;
+
+}
