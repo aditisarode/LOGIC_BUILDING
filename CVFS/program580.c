c193c8d49826da8ee6dda7b62de462e4aeff53c3
aditisarode <aditids2006@gmail.com>
2026-08-16 18:19:59 +0530

    CVFS Project

---
+++ CVFS/program580.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>            // only LINUX based OS
+#include <string.h>
+
+int main()
+{
+    int fd = 0;
+    int iRet = 0;
+    char Data[] = "Marvellous Infosystem";
+
+    fd = open("Marvellous.txt", O_RDWR | O_APPEND);
+
+    if(fd == -1)
+    {
+        printf("Unable to open file\n");
+    }
+    else
+    {
+        printf("File gets successfully opened with fd : %d\n", fd);
+
+        iRet = write(fd, Data, strlen(Data));
+
+        printf("%d bytes gets successfully written\n", iRet);
+
+        close(fd);
+    }
+
+    return 0;
+
+}
