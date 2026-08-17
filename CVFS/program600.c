4c93e6af29f3bb74259a4811288e31e4ee671a35
aditisarode <aditids2006@gmail.com>
2026-08-17 20:20:28 +0530

    CVFS Project

---
+++ CVFS/program600.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+
+#define BUFFER_SIZE 1024
+
+int CalculateFileSize(char FileName[])
+{
+    char Buffer[BUFFER_SIZE] = {'\0'};
+    int iRet = 0, fd = 0, iSize = 0;
+
+
+    fd = open(FileName, O_RDONLY);
+
+    if(fd == -1)
+    {
+        printf("Unable to open file\n");
+        return -1;
+    }
+    
+    while((iRet = read(fd, Buffer, sizeof(Buffer))) != 0)
+    {
+        iSize = iSize + iRet;
+    }
+
+    close(fd);
+    return iSize;
+    
+}
+
+int main()
+{   
+    char Fname[30] = {'\0'};
+    int iRet = 0;
+
+    printf("Enter the file name : \n");
+    scanf("%[^'\n']s", Fname);
+
+    iRet = CalculateFileSize(Fname);
+
+    printf("Size of file : %d bytes", iRet);
+    
+    return 0;
+}
