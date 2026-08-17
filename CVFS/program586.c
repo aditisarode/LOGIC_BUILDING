6f1863d9c05bbe56bde205760e33328d74e41844
aditisarode <aditids2006@gmail.com>
2026-08-17 20:17:55 +0530

    CVFS Project

---
+++ CVFS/program586.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>            // only LINUX based OS
+#include <string.h>
+
+#define BUFFER_SIZE 100
+
+int main()
+{
+    int fd = 0;
+    int iRet = 0;
+    char Data[BUFFER_SIZE] = {'\0'};
+
+    fd = open("Marvellous.txt", O_RDONLY);
+
+    if(fd == -1)
+    {
+        printf("Unable to open file\n");
+    }
+    else
+    {
+        printf("File gets successfully opened with fd : %d\n", fd);
+
+        iRet = read(fd, Data, 13);
+
+        printf("%d bytes gets successfully read\n", iRet);
+
+        printf("Data from file : %s\n", Data);
+
+        // Issue - Solved
+
+        memset(Data, '\0', sizeof(Data));
+
+        iRet = read(fd, Data, 3);
+
+        printf("%d bytes gets successfully read\n", iRet);
+
+        printf("Data from file : %s\n", Data);
+
+        close(fd);
+    }
+
+    return 0;
+
+}
