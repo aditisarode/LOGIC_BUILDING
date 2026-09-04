a9525429379833cecbdfc1e42e6b4821bd8486dc
aditisarode <aditids2006@gmail.com>
2026-09-04 21:51:37 +0530

    Problem 771 Solution

---
+++ Advanced_LB/program771.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class program771
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Enter the string : ");
+        String str = sobj.nextLine();
+
+        char Arr[] = str.toCharArray();
+
+        for(int i = 0; i < Arr.length; i++)
+        {
+            System.out.println(Arr[i]);
+        }
+
+    }
+}
