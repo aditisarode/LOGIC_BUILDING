223139379878000047d1f08e89bd7e49ad979b61
aditisarode <aditids2006@gmail.com>
2026-09-06 18:57:11 +0530

    Problem 790 Solution

---
+++ Advanced_LB/program790.java
@@ -0,0 +1,59 @@
+import java.util.*;
+
+class Matrix
+{
+    public int Arr[][];
+
+    public Matrix(int iRow, int iCol)
+    {
+        System.out.println("Inside Constructor");
+        Arr = new int [iRow][iCol];
+    }
+
+    protected void finalize()
+    {
+        System.out.println("Inside Finalize Method");
+        Arr = null;
+        System.gc();
+    }
+}
+
+class program790
+{
+    public static void main(String A[])
+    {
+       Scanner sobj = new Scanner(System.in);
+
+       int iRow = 0, iCol = 0;
+       int i = 0, j = 0;
+
+       System.out.println("Enter number of rows : ");
+       iRow = sobj.nextInt();
+
+       System.out.println("Enter number of columns : ");
+       iCol = sobj.nextInt();
+
+      Marvellous mobj = new Marvellous(iRow, iCol);
+       
+      /*  System.out.println("Enter the elements of matrix :");
+
+       for(i = 0; i < iRow; i++)
+       {
+            for(j = 0; j < iCol; j++)
+                {
+                    Arr[i][j] = sobj.nextInt();
+                }
+       }
+
+        System.out.println("Elements of the matrix :");
+
+        for(i = 0; i < iRow; i++)
+        {
+            for(j = 0; j < iCol; j++)
+                {
+                    System.out.print(Arr[i][j]+'\t');
+                }
+        } */
+
+    }
+}
