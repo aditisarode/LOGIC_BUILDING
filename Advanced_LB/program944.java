0ca1bffdc0d0f77b7a0030c0be610d4780b0acfd
aditisarode <aditids2006@gmail.com>
2026-09-22 10:58:40 +0530

    Problem 944 Solution

---
+++ Advanced_LB/program944.java
@@ -0,0 +1,11 @@
+
+class program944
+{
+    public static void main(String A[])
+    {
+        for(int i = 0; i < A.length; i++)
+        {
+            System.out.println(A[i]);
+        }
+    }
+}
