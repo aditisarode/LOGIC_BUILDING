a906c78934d9690ee7668a7bcbe7b2b3971fa18b
aditisarode <aditids2006@gmail.com>
2026-09-25 15:09:55 +0530

    Problem 967 Solution

---
+++ Advanced_LB/program967.java
@@ -0,0 +1,10 @@
+
+class program967
+{
+    public static void main(String A[])
+    {
+        System.out.println("Amit".compareTo("Amit"));
+        System.out.println("Amit".compareTo("Bhushan"));
+        System.out.println("Bhushan".compareTo("Amit"));
+    }
+}
